0f8ccab6c49cc7a5753a74683ea9e1cb4608bc44
Juan Pablo <juanpablolc414@gmail.com>
2024-04-15 19:39:51 -0500

    modelo de historial

--- obj/Debug/net8.0/Sistema.AssemblyInfo.cs
+++ obj/Debug/net8.0/Sistema.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sistema")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb3dee6ce3a525c08bac68657bd0ca3fefc8b8bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sistema")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sistema")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
