39e1fca55c9e31095fd94fee46405dd45bb3fb26
Ananias <amychelleoropezaortiz@gmail.com>
2024-04-16 12:52:31 -0500

    Estructura para el delete completada

--- obj/Debug/net8.0/Sistema.AssemblyInfo.cs
+++ obj/Debug/net8.0/Sistema.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sistema")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33c16bb9de396c65b61ea2762613efebdd69a92d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbdb4d041b952ca409c2ac5e41c816df19f1ac66")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sistema")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sistema")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
