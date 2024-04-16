265063b80971c798d78e1d9825787326b3b63953
Juan Pablo <juanpablolc414@gmail.com>
2024-04-15 20:54:47 -0500

    login and logout

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -3,20 +3,26 @@ using Microsoft.EntityFrameworkCore;
 using Sistema.Models;
 using Sistema.Data;
 
-namespace Sistema.Controllers{
-    public class EmployeesController : Controller{ 
+namespace Sistema.Controllers
+{
+    public class EmployeesController : Controller
+    {
         bool Sesion = false;
         int EmployeeId = 0;
         DateHistory newHistory = new DateHistory();
         private readonly ExamenContext _context;
 
-        public EmployeesController(ExamenContext context){
+        public EmployeesController(ExamenContext context)
+        {
             _context = context;
         }
 
-        public IActionResult Index(){
-            if(Sesion){
-                if(EmployeeId != 0){
+        public IActionResult Index()
+        {
+            if (Sesion)
+            {
+                if (EmployeeId != 0)
+                {
                     newHistory.Entries = DateTime.Now;
                     newHistory.EmployeesId = EmployeeId;
                     EmployeeId = 0;
@@ -27,18 +33,22 @@ namespace Sistema.Controllers{
             return RedirectToAction("Login");
         }
 
-        public IActionResult Login(string Email, string Password){
+        public IActionResult Login(string Email, string Password)
+        {
 
-            if(Sesion){
+            if (Sesion)
+            {
                 return RedirectToAction("Index");
             }
 
             var employees = from employee in _context.Employees select employee;
 
-            if(!string.IsNullOrEmpty(Email) && !string.IsNullOrEmpty(Email)){
+            if (!string.IsNullOrEmpty(Email) && !string.IsNullOrEmpty(Email))
+            {
                 employees.Where(x => x.Email == Email && x.Password == Password);
 
-                if(employees.Count() > 0){
+                if (employees.Count() > 0)
+                {
                     Sesion = true;
                     EmployeeId = employees.First().Id;
                     return RedirectToAction("Index");
@@ -48,7 +58,8 @@ namespace Sistema.Controllers{
             return View();
         }
 
-        public IActionResult Logout(){
+        public IActionResult Logout()
+        {
             Sesion = false;
             newHistory.Exits = DateTime.Now;
             _context.DateHistories.Add(newHistory);
