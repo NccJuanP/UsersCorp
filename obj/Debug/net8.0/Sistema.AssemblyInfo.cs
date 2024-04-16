1cd3c355436dc375539ad966a6fe073bc8b4d664
Ananias <amychelleoropezaortiz@gmail.com>
2024-04-16 13:00:04 -0500

    Espacio de user lista.

--- obj/Debug/net8.0/Sistema.AssemblyInfo.cs
+++ obj/Debug/net8.0/Sistema.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sistema")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbdb4d041b952ca409c2ac5e41c816df19f1ac66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39e1fca55c9e31095fd94fee46405dd45bb3fb26")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sistema")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sistema")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
