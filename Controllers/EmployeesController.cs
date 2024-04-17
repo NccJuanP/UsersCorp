ad4448ada46bb804f0649356bb298764fa3671d4
julianroman1990 <julianroman1990@gmail.com>
2024-04-17 16:10:55 -0500

    crud terminado sin update

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Sistema.Models;
 using Sistema.Data;
 using Newtonsoft.Json;
+using Microsoft.EntityFrameworkCore;
 
 namespace Sistema.Controllers
 {
@@ -25,17 +26,17 @@ namespace Sistema.Controllers
             string? sesion = HttpContext.Session.GetString("sesion");
             if (sesion != null && sesion.Equals("si"))
             {
-                if (HttpContext.Session.GetString("jornada") != null)
+                    var employee = JsonConvert.DeserializeObject<Employee>(HttpContext.Session.GetString("empleado"));
+                if (HttpContext.Session.GetString("jornada").Equals("1"))
                 {   
                     DateHistory newHistory = new DateHistory();
-                    var employee = JsonConvert.DeserializeObject<Employee>(HttpContext.Session.GetString("empleado"));
                     newHistory.Entries = DateTime.Now;
                     newHistory.EmployeesId = employee.Id;
                     string historyJson = JsonConvert.SerializeObject(newHistory);
                     HttpContext.Session.SetString("history", historyJson);
-                    HttpContext.Session.Remove("jornada");
+                    HttpContext.Session.SetString("jornada", "0");
                 }
-                return View(_context.Employees.ToList());
+                return View(employee);
             }
             else{
             return RedirectToAction("Login");
@@ -46,8 +47,8 @@ namespace Sistema.Controllers
 
         public IActionResult Login(string Email, string Password)
         {
-
-            if (HttpContext.Session.GetString("sesion") != null)
+            var sesion = HttpContext.Session.GetString("sesion");
+            if (sesion != null && sesion.Equals("si"))
             {
                 return RedirectToAction("Index");
             }
@@ -56,8 +57,7 @@ namespace Sistema.Controllers
 
             if (!string.IsNullOrEmpty(Email) && !string.IsNullOrEmpty(Email))
             {
-                employees.Where(x => x.Email == Email && x.Password == Password);
-
+                employees = employees.Where(x => x.Email.Equals(Email) && x.Password.Equals(Password));
                 if (employees.Count() > 0)
                 {
                     HttpContext.Session.SetString("sesion", "si");
@@ -84,20 +84,93 @@ namespace Sistema.Controllers
                 }
                 return RedirectToAction("Index");
         }
+        [HttpGet]
         public IActionResult Register(){
+           
             return View();
         }
-        public IActionResult Details(){
+
+        [HttpPost]
+        public async Task <IActionResult> Create(Employee employee){
+            if(ModelState.IsValid){
+                await _context.Employees.AddAsync(employee);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
+        }
+        public IActionResult Details(int? id){
+            string? sesion = HttpContext.Session.GetString("sesion");
+            if (sesion != null && sesion.Equals("si"))
+            {
+            if(id != null){
+                var user = _context.Employees.Find(id);
+                var registros = from register in _context.DateHistory select register;
+                registros = registros.Where(x => x.EmployeesId == user.Id);
+                ViewData["id"] = user.Id;
+                ViewData["name"] = user.Name;
+                ViewData["Lastnames"] = user.LastNames;
+                ViewData["email"] = user.Email;
+                ViewData["phone"] = user.Phone;
+                return View(registros);
+            }
+            return RedirectToAction("Index");
+            }else{
+                return RedirectToAction("Index");
+            }
+            
+            
+        }
+        public IActionResult Delete(int? id){
+            string? sesion = HttpContext.Session.GetString("sesion");
+            if (sesion != null && sesion.Equals("si"))
+            {
+            if(id !=  null){
+                var user = _context.Employees.Find(id);
+            return View(user);
+            }
+            return RedirectToAction("Index");
+            }else{
+                return RedirectToAction("Index");
+            }
+            
+        }
+        public async Task <IActionResult> confirmDelete(int id){
+            var registros = from registro in _context.DateHistory where registro.EmployeesId == id select registro;
+            _context.DateHistory.RemoveRange(registros);
+            _context.Employees.Remove(_context.Employees.Find(id));
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        public async Task <IActionResult> Update(int id){
+            string? sesion = HttpContext.Session.GetString("sesion");
+            if (sesion != null && sesion.Equals("si"))
+            {
+           var user = await _context.Employees.FindAsync(id);
+            return View(user);
+            }else{
+                return RedirectToAction("Index");
+            }
+            
+        }
+
+        public async Task <IActionResult> confirmUpdate(int id, Employee employee){
+            if(ModelState.IsValid){
+                _context.Employees.Update(employee);
+                return RedirectToAction("Index");
+            }
             return View();
         }
-        public IActionResult Delete(){
-            return View();
-        }
-        public IActionResult Update(){
-            return View();
-        }
-        public IActionResult User(){
-            return View();
+
+        public async Task <IActionResult> User(){
+            string? sesion = HttpContext.Session.GetString("sesion");
+            if (sesion != null && sesion.Equals("si"))
+            {
+            var user = await _context.Employees.ToListAsync();
+            return View(user);
+            }else{
+                return RedirectToAction("Index");
+            }
+            
         }
     }
 }
